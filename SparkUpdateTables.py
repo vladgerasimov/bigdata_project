a7ad048f5163611e9f55df5758812c5eb03c4c5d
Vlad <68383954+vladgerasimov@users.noreply.github.com>
2024-01-14 11:05:17 +0400

    bot discount percent (#3)
    * bot discount message added
    
    * fix
    
    * bot fix
    
    * bot fix
    
    * spark code updated
    
    ---------
    
    Co-authored-by: vgerasimov <vgerasimov@quantumbrains.com>

--- SparkUpdateTables.py
+++ SparkUpdateTables.py
@@ -1,113 +1,73 @@
+import pyspark.sql.functions as F
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, LongType, StringType
 
 
 
-def update_df_link_vendor_code(data_to_update):
-        spark = SparkSession.builder\
-                .master("local[*]")\
-                .appName('yurkin_create_tables')\
-                .getOrCreate()
-        
+def update_df_link_vendor_code(data_to_update, spark):
         schema_df_link_vendor_code = StructType([
-                                        StructField("link", StringType(), True),
-                                        StructField("vendor_code", LongType(), True),
-                                        StructField("goods_name", StringType(), True)
-                                        ])
-        
-        df = spark.createDataFrame(data_to_update, schema=schema_df_link_vendor_code)
+            StructField("link", StringType(), True),
+            StructField("vendor_code", LongType(), True),
+            StructField("goods_name", StringType(), True)
+        ])
+        df = spark.createDataFrame([data_to_update], schema=schema_df_link_vendor_code)
 
         df.write.mode('append').parquet("hdfs:///user/andreyyur/project/df_link_vendor_code.parquet")
 
-        spark.stop()
 
-
-def update_df_user_vendor_code(data_to_update):
-        spark = SparkSession.builder\
-                .master("local[*]")\
-                .appName('yurkin_create_tables')\
-                .getOrCreate()
-
-        print("------> spark alive")
+def update_df_user_vendor_code(data_to_update, spark):
         schema_df_user_vendor_code = StructType([
-                                                StructField("user_id", LongType(), True),
-                                                StructField("vendor_code", LongType(), True),
-                                                StructField("discount_percent", IntegerType(), True)
-                                                ])
-
-        df = spark.createDataFrame(data_to_update, schema=schema_df_user_vendor_code)
-
+            StructField("user_id", LongType(), True),
+            StructField("vendor_code", LongType(), True),
+            StructField("discount_percent", IntegerType(), True)
+        ])
+        df = spark.createDataFrame([data_to_update], schema=schema_df_user_vendor_code)
         print("------> df created")
         df.write.mode('append').parquet("hdfs:///user/andreyyur/project/df_user_vendor_code.parquet")
 
-        spark.stop()
 
-
-def check_df_user_vendor_code(data_to_check):
-        spark = SparkSession.builder\
-                .master("local[*]")\
-                .appName('yurkin_create_tables')\
-                .getOrCreate()
-
+def check_df_user_vendor_code(data_to_check, spark) -> int:
         existing_df = spark.read.parquet("hdfs:///user/andreyyur/project/df_user_vendor_code.parquet")
-        existing_df.createOrReplaceTempView("existing_df_view")
+        user_id, vendor_code, discount_percent, *_ = data_to_check
+        if discount_percent:
+            discount_condition = F.col("discount_percent") == F.lit(discount_percent)
+        else:
+            discount_condition = F.col("discount_percent").isNull() | F.isnan(F.col("discount_percent"))
 
-        query = f"""
-                SELECT COUNT(*) as count_check
-                FROM existing_df_view
-                WHERE user_id = {data_to_check[0][0]}
-                AND vendor_code = {data_to_check[0][1]}
-                """
-        count_df = spark.sql(query)
+        rows_count = (
+            existing_df
+            .filter(
+                (F.col("user_id") == F.lit(user_id))
+                & (F.col("vendor_code") == F.lit(vendor_code))
+                & discount_condition
+            ).count()
+        )
 
-        count_value = count_df.first().asDict()["count_check"]
+        return rows_count
 
-        spark.stop()
-
-        return count_value
-
-
-def check_df_link_vendor_code(data_to_check):
-        spark = SparkSession.builder\
-                .master("local[*]")\
-                .appName('yurkin_create_tables')\
-                .getOrCreate()
-
+def check_df_link_vendor_code(data_to_check, spark):
         existing_df = spark.read.parquet("hdfs:///user/andreyyur/project/df_link_vendor_code.parquet")
-        existing_df.createOrReplaceTempView("existing_df_view")
+        link, vendor_code, goods_name = data_to_check
 
-        query = f"""
-                SELECT COUNT(*) as count_check
-                FROM existing_df_view
-                WHERE link = '{data_to_check[0][0]}'
-                AND vendor_code = {data_to_check[0][1]}
-                AND goods_name = '{data_to_check[0][2]}'
-                """
-        count_df = spark.sql(query)
+        rows_count = (
+            existing_df
+            .filter(
+                (F.col("link") == F.lit(link))
+                & (F.col("vendor_code") == F.lit(vendor_code))
+                & (F.col("goods_name") == F.lit(goods_name))
+            ).count()
+        )
 
-        count_value = count_df.first().asDict()["count_check"]
-
-        spark.stop()
-
-        return count_value
+        return rows_count
 
 
-def update_df_prices_history(data_to_update):
-        spark = SparkSession.builder\
-                .master("local[*]")\
-                .appName('yurkin_create_tables')\
-                .getOrCreate()
-        
-        # columns = ["vendor_code", "price", "datetime"]
+def update_df_prices_history(data_to_update, spark):
         schema = StructType([
-                        StructField("vendor_code", LongType(), True),
-                        StructField("price", LongType(), True),
-                        StructField("datetime", StringType(), True)
-                        ])
+            StructField("vendor_code", LongType(), True),
+            StructField("price", LongType(), True),
+            StructField("datetime", StringType(), True)
+        ])
 
-        rdd = spark.sparkContext.parallelize(data_to_update)
-        df = spark.createDataFrame(rdd, schema)
+        df = spark.createDataFrame([data_to_update], schema=schema)
 
         df.write.mode('append').parquet("hdfs:///user/andreyyur/project/df_prices_history.parquet")
-
-        spark.stop()
